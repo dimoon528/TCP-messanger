5688847f94ca484e70779a962a0fc2e178343e2b
dimoon528 <124904226+dimoon528@users.noreply.github.com>
2024-11-29 10:14:49 +0400

    Add files via upload

---
+++ TCP-Client/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace TCP_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(398, 110);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(395, 90);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Имя:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(31, 383);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(734, 60);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(799, 399);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(21, 110);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(359, 265);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox4.Location = new System.Drawing.Point(560, 110);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(338, 265);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "IP сервера: ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(124, 15);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(132, 22);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(124, 47);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(132, 22);
+            this.textBox6.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 50);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Port сервера:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 90);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Сообщения";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(557, 90);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Закодированные";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(398, 149);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 28);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Подключиться";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(924, 456);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TCP messanger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TCP-Client/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TCP_Client
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<char, string> cyrillicToIso = new Dictionary<char, string>()
+        {
+        {'А', "0xC0"}, {'Б', "0xC1"}, {'В', "0xC2"}, {'Г', "0xC3"},
+        {'Д', "0xC4"}, {'Е', "0xC5"}, {'Ё', "0xA1"}, {'Ж', "0xC6"},
+        {'З', "0xC7"}, {'И', "0xC8"}, {'Й', "0xC9"}, {'К', "0xCA"},
+        {'Л', "0xCB"}, {'М', "0xCC"}, {'Н', "0xCD"}, {'О', "0xCE"},
+        {'П', "0xCF"}, {'Р', "0xD0"}, {'С', "0xD1"}, {'Т', "0xD2"},
+        {'У', "0xD3"}, {'Ф', "0xD4"}, {'Х', "0xD5"}, {'Ц', "0xD6"},
+        {'Ч', "0xD7"}, {'Ш', "0xD8"}, {'Щ', "0xD9"}, {'Ъ', "0xDA"},
+        {'Ы', "0xDB"}, {'Ь', "0xDC"}, {'Э', "0xDD"}, {'Ю', "0xDE"},
+        {'Я', "0xDF"}, {'а', "0xE0"}, {'б', "0xE1"}, {'в', "0xE2"},
+        {'г', "0xE3"}, {'д', "0xE4"}, {'е', "0xE5"}, {'ё', "0xB5"},
+        {'ж', "0xE6"}, {'з', "0xE7"}, {'и', "0xE8"}, {'й', "0xE9"},
+        {'к', "0xEA"}, {'л', "0xEB"}, {'м', "0xEC"}, {'н', "0xED"},
+        {'о', "0xEE"}, {'п', "0xEF"}, {'р', "0xF0"}, {'с', "0xF1"},
+        {'т', "0xF2"}, {'у', "0xF3"}, {'ф', "0xF4"}, {'х', "0xF5"},
+        {'ц', "0xF6"}, {'ч', "0xF7"}, {'ш', "0xF8"}, {'щ', "0xF9"},
+        {'ъ', "0xFA"}, {'ы', "0xFB"}, {'ь', "0xFC"}, {'э', "0xFD"},
+        {'ю', "0xFE"}, {'я', "0xFF"},
+        {'0', "0x30"}, {'1', "0x31"}, {'2', "0x32"}, {'3', "0x33"},
+        {'4', "0x34"}, {'5', "0x35"}, {'6', "0x36"}, {'7', "0x37"},
+        {'8', "0x38"}, {'9', "0x39"},
+        {'.', "0x2E"}, {',', "0x2C"}, {'!', "0x21"}, {'?', "0x3F"},
+        {';', "0x3B"}, {':', "0x3A"}, {'-', "0x2D"}, {'(', "0x28"},
+        {')', "0x29"}, {'[', "0x5B"}, {']', "0x5D"}, {'^', "0x5E"},
+        {'*', "0x2A"}, {'+', "0x2B"}, {'=', "0x3D"}, {'<', "0x3C"},
+        {'>', "0x3E"}, {' ', "0x20"}
+
+        };
+
+        Dictionary<string, char> isoToCyrillic = new Dictionary<string, char>()
+          {
+            {"0xC0", 'А'}, {"0xC1", 'Б'}, {"0xC2", 'В'}, {"0xC3", 'Г'},
+            {"0xC4", 'Д'}, {"0xC5", 'Е'}, {"0xA1", 'Ё'}, {"0xC6", 'Ж'},
+            {"0xC7", 'З'}, {"0xC8", 'И'}, {"0xC9", 'Й'}, {"0xCA", 'К'},
+            {"0xCB", 'Л'}, {"0xCC", 'М'}, {"0xCD", 'Н'}, {"0xCE", 'О'},
+            {"0xCF", 'П'}, {"0xD0", 'Р'}, {"0xD1", 'С'}, {"0xD2", 'Т'},
+            {"0xD3", 'У'}, {"0xD4", 'Ф'}, {"0xD5", 'Х'}, {"0xD6", 'Ц'},
+            {"0xD7", 'Ч'}, {"0xD8", 'Ш'}, {"0xD9", 'Щ'}, {"0xDA", 'Ъ'},
+            {"0xDB", 'Ы'}, {"0xDC", 'Ь'}, {"0xDD", 'Э'}, {"0xDE", 'Ю'},
+            {"0xDF", 'Я'}, {"0xE0", 'а'}, {"0xE1", 'б'}, {"0xE2", 'в'},
+            {"0xE3", 'г'}, {"0xE4", 'д'}, {"0xE5", 'е'}, {"0xB5", 'ё'},
+            {"0xE6", 'ж'}, {"0xE7", 'з'}, {"0xE8", 'и'}, {"0xE9", 'й'},
+            {"0xEA", 'к'}, {"0xEB", 'л'}, {"0xEC", 'м'}, {"0xED", 'н'},
+            {"0xEE", 'о'}, {"0xEF", 'п'}, {"0xF0", 'р'}, {"0xF1", 'с'},
+            {"0xF2", 'т'}, {"0xF3", 'у'}, {"0xF4", 'ф'}, {"0xF5", 'х'},
+            {"0xF6", 'ц'}, {"0xF7", 'ч'}, {"0xF8", 'ш'}, {"0xF9", 'щ'},
+            {"0xFA", 'ъ'}, {"0xFB", 'ы'}, {"0xFC", 'ь'}, {"0xFD", 'э'},
+            {"0xFE", 'ю'}, {"0xFF", 'я'},
+            { "0x30", '0' }, { "0x31", '1' }, { "0x32", '2' }, { "0x33", '3' },
+            { "0x34", '4' }, { "0x35", '5' }, { "0x36", '6' }, { "0x37", '7' },
+            { "0x38", '8' }, { "0x39", '9' }, { "0x2E", '.' }, { "0x2C", ',' },
+            { "0x21", '!' }, { "0x3F", '?' }, { "0x3B", ';' }, { "0x3A", ':' },
+            { "0x2D", '-' }, { "0x28", '(' }, { "0x29", ')' }, { "0x5B", '[' },
+            { "0x5D", ']' }, { "0x5E", '^' }, { "0x2A", '*' }, { "0x2B", '+' },
+            { "0x3D", '=' }, { "0x3C", '<' }, { "0x3E", '>' }, { "0x20", ' ' }
+        };
+
+        // Метод для кодирования текста в ISO 8859-5 и двоичный вид
+        public string[] encode(string text)
+        {
+            // Инициализация массива для хранения результатов: закодированный текст и его двоичное представление
+            string[] res = { "", "" };
+
+            // Получение длины строки
+            int strLength = text.Length;
+
+            // Перебор каждого символа в строке
+            for (int i = 0; i < strLength; i++)
+            {
+                // Проверка, есть ли символ в словаре cyrillicToIso
+                if (cyrillicToIso.ContainsKey(text[i]))
+                {
+                    // Добавление закодированного символа в первый элемент массива
+                    res[0] += cyrillicToIso[text[i]];
+                    // Добавление двоичного представления символа во второй элемент массива
+                    res[1] += Convert.ToString(text[i], 2);
+                    // Добавление пробела после каждого двоичного представления символа
+                    res[1] += " ";
+                }
+            }
+
+            // Возвращение массива с закодированным текстом и его двоичным представлением
+            return res;
+        }
+
+        // Метод для декодирования текста из ISO 8859-5
+        public string decode(string text)
+        {
+            // Инициализация строки для хранения разделенного текста
+            string spltd = "";
+            // Получение длины строки
+            int strLength = text.Length;
+
+            // Перебор каждого символа в строке
+            for (int i = 0; i < strLength; i++)
+            {
+                // Добавление пробела после каждого четвертого символа
+                if (i % 4 == 0)
+                {
+                    spltd += " ";
+                }
+                // Добавление символа в строку
+                spltd += text[i];
+            }
+
+            // Инициализация строки для хранения декодированного текста
+            string res = "";
+            // Разделение строки на массив подстрок по пробелам
+            string[] test = spltd.Split(' ');
+
+            // Перебор каждой подстроки в массиве
+            foreach (string i in test)
+            {
+                // Проверка, есть ли подстрока в словаре isoToCyrillic
+                if (isoToCyrillic.ContainsKey(i))
+                {
+                    // Добавление декодированного символа в строку
+                    res += isoToCyrillic[i];
+                }
+            }
+
+            // Возвращение декодированного текста
+            return res;
+        }
+
+        // Объявление переменных для хранения IP-адреса сервера, порта сервера и сокета клиента
+        string serverIp;
+        int serverPort;
+        Socket client;
+
+        // Объявление переменной для хранения локального порта
+        int localPort = Convert.ToInt32("11006");
+
+        // Объявление переменной для хранения имени пользователя
+        string name;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            // Объявление переменной для хранения локального IP-адреса
+            string localIP;
+
+            // Использование сокета для получения локального IP-адреса
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                // Подключение к удаленному серверу для получения локального IP-адреса
+                socket.Connect("8.8.8.8", 65530);
+                // Получение локального IP-адреса
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            // Отображение локального IP-адреса в текстовом поле
+            textBox5.Text = localIP;
+            // Отображение локального порта в текстовом поле
+            textBox6.Text = "11006";
+
+            // Создание конечной точки для сервера
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(localIP), localPort);
+            // Создание сокета сервера
+            Socket server = new Socket(
+                ipEndPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            // Привязка сокета сервера к конечной точке
+            server.Bind(ipEndPoint);
+            // Прослушивание входящих соединений
+            server.Listen(10);
+
+            // Принятие входящего соединения
+            Socket handler = await server.AcceptAsync();
+            // Бесконечный цикл для обработки входящих сообщений
+            while (true)
+            {
+                // Создание буфера для хранения входящего сообщения
+                Byte[] buffer = new byte[1024];
+                // Получение входящего сообщения
+                int message = await handler.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
+                // Декодирование входящего сообщения
+                string encoded = Encoding.UTF8.GetString(buffer, 0, message);
+
+                // Разделение сообщения на части
+                string[] test = encoded.Split(new string[] { "0x3A" }, StringSplitOptions.None);
+
+                // Отображение декодированного сообщения в текстовом поле
+                textBox3.Text += Environment.NewLine + decode(encoded);
+
+                // Отображение декодированного имени отправителя и сообщения в текстовом поле
+                textBox4.Text += Environment.NewLine + decode(test[0]) + ": " + test[1];
+                // Отображение декодированного имени отправителя и закодированного сообщения в текстовом поле
+                textBox4.Text += Environment.NewLine + decode(test[0]) + ": " + encode(test[1])[1];
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            // Проверка, что имя пользователя не пустое
+            if (textBox1.Text.Length > 0)
+            {
+                // Получение IP-адреса сервера из текстового поля
+                serverIp = textBox5.Text;
+                // Получение порта сервера из текстового поля
+                serverPort = Convert.ToInt32(textBox6.Text);
+
+                // Получение имени пользователя из текстового поля
+                name = textBox1.Text;
+
+                // Активация кнопки отправки сообщения
+                button1.Enabled = true;
+
+                // Создание конечной точки для клиента
+                IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+                // Создание сокета клиента
+                client = new Socket(
+                    ipEndPoint.AddressFamily,
+                    SocketType.Stream,
+                    ProtocolType.Tcp);
+
+                // Подключение к серверу
+                await client.ConnectAsync(ipEndPoint);
+            }
+        }
+
+        // Обработчик события нажатия кнопки отправки сообщения
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // Получение сообщения из текстового поля
+            string message = textBox2.Text;
+            // Кодирование сообщения
+            byte[] messageBytes = encode(name + ": " + message)[0].Select(s => (byte)s).ToArray();
+            // Отправка сообщения на сервер
+            await client.SendAsync(new ArraySegment<byte>(messageBytes), SocketFlags.None);
+
+            // Отображение отправленного сообщения в текстовом поле
+            textBox3.Text += Environment.NewLine + name + ": " + message;
+
+            // Отображение закодированного сообщения в текстовом поле
+            textBox4.Text += Environment.NewLine + name + ": " + encode(message)[0];
+            // Отображение двоичного представления сообщения в текстовом поле
+            textBox4.Text += Environment.NewLine + name + ": " + encode(message)[1];
+        }
+    }
+}
